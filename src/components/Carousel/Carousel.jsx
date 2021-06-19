dcffb4aca7344c1b4e0a9a6987a50553f01010b3
Gustavo <gustavo.prog40@gmail.com>
2021-06-19 11:10:45 -0300

    second

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 // import Carousel from 'react-elastic-carousel'
 import { Carousel } from 'react-responsive-carousel'
 import PC1 from '../../assets/pc-1.jpg'
+import PC2 from '../../assets/pc-2.jpg'
+import PC3 from '../../assets/pc-3.jpg'
 import { ContainerCarousel } from './style'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
@@ -9,9 +11,8 @@ class App extends Component {
   state = {
     items: [
       { id: 1, embed: `${PC1}` },
-      { id: 2, embed: `${PC1}` },
-      { id: 3, embed: `${PC1}` },
-      { id: 4, embed: `${PC1}` },
+      { id: 2, embed: `${PC2}` },
+      { id: 3, embed: `${PC3}` },
     ],
   }
 
@@ -26,7 +27,7 @@ class App extends Component {
           stopOnHover
           showArrows
           infiniteLoop
-          transitionTime={1000}
+          transitionTime={1200}
         >
           {items.map((item) => (
             <div key={item.id}>
