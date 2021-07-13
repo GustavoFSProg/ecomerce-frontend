78dda94a4be95b142f5c0960409401fd9e231c9a
Gustavo <gustavo.prog40@gmail.com>
2021-07-13 10:41:31 -0300

    cart-2

--- src/components/Cart/components/Main.js
+++ src/components/Cart/components/Main.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Product from './Product';
+import React from 'react'
+import Product from './Product'
+import '../index.css'
 
 export default function Main(props) {
-  const { products, onAdd } = props;
+  const { products, onAdd } = props
   return (
     <main className="block col-2">
       <h2>Products</h2>
       <div className="row">
         {products.map((product) => (
-          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+          <div className="list-product">
+            <Product key={product.id} product={product} onAdd={onAdd}></Product>
+          </div>
         ))}
       </div>
     </main>
-  );
+  )
 }
